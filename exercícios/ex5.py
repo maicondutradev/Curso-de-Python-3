45e2560c514d2a5733ce8bfb7517d6674028ca7d
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-10-14 07:49:45 -0300

    ex5

--- exercícios/ex5.py
+++ exercícios/ex5.py
@@ -1,6 +1,11 @@
 nome = 'Maicon Dutra'
 
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
-print(nome[3])
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += '*' + letra 
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
