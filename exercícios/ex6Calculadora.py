af26cd7d54bf84690ae1637d0f3b6c88eed60927
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-10-14 08:10:05 -0300

    ex6Calculadora

--- exercícios/ex6Calculadora.py
+++ exercícios/ex6Calculadora.py
@@ -2,7 +2,7 @@ try:
     while True:
         numero1 = float(input('Digite o primeiro número: '))
         numero2 = float(input('Digite o segundo número: '))
-        simbolo = input('Digite o calculo que você deseja executar Adição(+) Subtração(-) Divisão(/) Multiplicação(*) Sair(#): ')
+        simbolo = input('Digite o calculo que você deseja executar Adição(+) Subtração(-) Divisão(/) Multiplicação(*): ')
 
         if simbolo == '+':
             adicao = numero1 + numero2
@@ -16,7 +16,12 @@ try:
         elif simbolo == '*':
             multiplicacao = numero1 * numero2
             print(f'A multiplicação dos números {numero1} * {numero2} é {multiplicacao}')
-        elif simbolo == '#':
+
+        sair = input('Quer sair? [S]sim ou [N]não: ')
+        if sair == 'S':
+            print('Calculadora finalizada!')
             break
+        elif sair == 'N':
+            continue
 except:
     print('Não é um número ou simbolo!')
