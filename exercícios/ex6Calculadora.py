629d51e369a24464fcebc6f8caa7ddca519c9f0d
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-10-14 08:54:07 -0300

    ex6CalculadoraFinal

--- exercícios/ex6Calculadora.py
+++ exercícios/ex6Calculadora.py
@@ -1,27 +1,39 @@
+from time import sleep
+
 try:
     while True:
+        delay = 2
         numero1 = float(input('Digite o primeiro número: '))
         numero2 = float(input('Digite o segundo número: '))
         simbolo = input('Digite o calculo que você deseja executar Adição(+) Subtração(-) Divisão(/) Multiplicação(*): ')
 
         if simbolo == '+':
             adicao = numero1 + numero2
+            print('Realizando a sua conta!Espere por favor!')
+            sleep(delay)
             print(f'A adição dos números {numero1} + {numero2} é {adicao}')
         elif simbolo == '-':
             subtracao = numero1 - numero2
+            print('Realizando a sua conta!Espere por favor!')
+            sleep(delay)
             print(f'A subtração dos números {numero1} - {numero2} é {subtracao}')
         elif simbolo == '/':
             divisao = numero1 / numero2
+            print('Realizando a sua conta!Espere por favor!')
+            sleep(delay)
             print(f'A divisão dos números {numero1} / {numero2} é {divisao}')
         elif simbolo == '*':
             multiplicacao = numero1 * numero2
+            print('Realizando a sua conta!Espere por favor!')
+            sleep(delay)
             print(f'A multiplicação dos números {numero1} * {numero2} é {multiplicacao}')
 
         sair = input('Quer sair? [S]sim ou [N]não: ')
-        if sair == 'S':
+        sair = sair.lower()
+        if sair == 's':
             print('Calculadora finalizada!')
             break
-        elif sair == 'N':
+        elif sair == 'n':
             continue
 except:
     print('Não é um número ou simbolo!')
