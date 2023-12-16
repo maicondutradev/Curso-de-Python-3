46938eafcae40833aaffd831b25c6e50654d2405
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-16 09:23:21 -0300

    aula 01

---
+++ python_intermediário/funções/aula01.py
@@ -0,0 +1,34 @@
+#def Print():
+#    print('Hello World')
+
+#Print()
+
+#-----------------------------------------------
+
+#def imprimir(a, b, c):
+#    print(a, b, c)
+
+#imprimir(1, 2, 3)
+#imprimir(4, 5, 6)
+
+#-----------------------------------------------
+
+#def saudacao(nome):
+#    print(f'Olá {nome}!')
+
+#while True: 
+#    nome = input('Digite seu nome: ')
+
+#    saudacao(nome)
+
+#------------------------------------------------
+
+#def multiplo_de(numero, multiplo):
+#    resultado = numero % multiplo == 0
+#    print(f'{numero} é múltiplo de {multiplo}?', end=' ')
+#    print(resultado)
+ 
+ 
+#multiplo_de(16, 8)
+#multiplo_de(15, 3)
+#multiplo_de(10, 2)
